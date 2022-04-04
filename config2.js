10026e593a22e2637f848c405edab301c727b1b4
jpbanday <jypnbanday@gmail.com>
2022-04-04 17:25:51 +0800

    fixed database and services

--- config2.js
+++ config2.js
@@ -1,21 +1,10 @@
 const init = require('firebase/app');
 const auth = require('firebase/auth');
-const fs = require('firebase-admin/app')
-const fafs = require('firebase-admin/firestore')
+const fs = require('firebase/firestore');
 var async = require('async');
 var serviceAccount = require('./admin.json');
+const admin = require("firebase-admin")
 
-// fs.initializeApp({
-// 	apiKey: "AIzaSyDW77h9ekpzn_KdBst8M8G2mRgT9Nb0z4M",
-// 	authDomain: "actualproject-ea38f.firebaseapp.com",
-// 	databaseURL: "https://actualproject-ea38f-default-rtdb.asia-southeast1.firebasedatabase.app",
-// 	projectId: "actualproject-ea38f",
-// 	storageBucket: "actualproject-ea38f.appspot.com",
-// 	messagingSenderId: "886053792463",
-// 	appId: "1:886053792463:web:030b8456cfbed1d59625ba",
-// 	measurementId: "G-5PV00PPFGK",
-// 	credential: fs.credential.cert(serviceAccount)
-// });
 const firebaseConfig = {
 	apiKey: "AIzaSyDW77h9ekpzn_KdBst8M8G2mRgT9Nb0z4M",
 	authDomain: "actualproject-ea38f.firebaseapp.com",
@@ -29,9 +18,10 @@ const firebaseConfig = {
 };
 
 const newApp = init.initializeApp(firebaseConfig);
+const db = fs.getFirestore();
 const newAuth = auth.getAuth(newApp);
-// const getAuth = auth.getAuth();
-//auth.connectAuthEmulator(newAuth, "http://localhost:8080");
+// // const getAuth = auth.getAuth();
+// //auth.connectAuthEmulator(newAuth, "http://localhost:8080");
 
 auth.onAuthStateChanged(newAuth, (user) => {
 	if (user) {
@@ -45,18 +35,105 @@ auth.onAuthStateChanged(newAuth, (user) => {
 	}
 });
 
-// async function insertData (collection, doc, obj) {
+async function insertDataUser (collection, /*doc,*/ obj) { // FOR SIGNUP USER
+	try {
+		const dataRef = fs.collection(db, collection);
+		await fs.setDoc(fs.doc(dataRef, obj.uid), obj);
+		return true;
+	} catch(err) {
+		console.log('err save', err)
+		return false;
+	}
+}
+
+async function insertData (collection, obj) { // INSERT DATA EXCEPT FOR SIGN UP
+	try {
+		let dataRef = fs.doc(fs.collection(db, collection))
+		await fs.setDoc(dataRef, obj)
+		// const dataRef = fs.collection(db, collection);
+		// var id = fs.collection(db, collection).document().getId();
+		// console.log('id', id)
+		// await fs.setDoc(fs.doc(dataRef, obj.uid), obj);
+		return true;
+	} catch(err) {
+		console.log('err save', err)
+		return false;
+	}
+}
+
+async function getDataUser (collection, uid, last_logged_in) {
+	try {
+		const dataRef = await fs.doc(db, collection, uid);
+		const docSnap = await fs.getDoc(dataRef);
+
+		// UPDATE WITH LAST LOGGED IN
+		const dataRef2 = fs.collection(db, collection);
+		var data = docSnap.data()
+		var obj = {
+			last_logged_in,
+			username: data.username,
+			uid: data.uid,
+			name: data.name,
+			email: data.email
+		}
+		await fs.setDoc(fs.doc(dataRef2, uid), obj);
+
+		return { success: true, message: 'Data found!', key: docSnap.data() }
+	} catch(err) {
+		console.log('err', err)
+		return false;
+	}
+}
+
+async function getData (collection) { // LIST OR BY ID
+	try {
+		let dataRef = await fs.getDocs(fs.collection(db, collection));
+		var arr = [];
+		dataRef.forEach((doc) => {
+			var obj = doc.data();
+			obj['id'] = doc.id;
+			arr.push(obj);
+		});
+		return { success: true, message: 'Data found!', key: arr }
+	} catch(err) {
+		console.log('err', err)
+		return false;
+	}
+}
+
+async function updateData(collection, obj) {
+	try {
+		const dataRef = fs.doc(db, collection, obj.details.id);
+		await fs.updateDoc(dataRef, obj.details);
+		return { success: true, message: 'Updated!', key: null }
+	} catch(err) {
+		console.log('err', err)
+		return false;
+	}
+}
+
+async function deleteData (collection, id){
+	try {
+		await fs.deleteDoc(fs.doc(db, collection, id))
+		return { success: true, message: 'Updated!', key: null }
+	} catch(err) {
+		console.log('err', err)
+		return false;
+	} 
+}
+
+// async function deleteUserData (collection, id) {
 // 	try {
-// 		console.log('collection, doc, obj', collection, doc, obj)
-		
-// 		const docRef = db.collection('User').doc(obj.uid);
-// 		await docRef.set(obj);
-// 		return true;
+// 		auth.get
+// 		// const user = newAuth;
+// 		// console.log('user', user)
 // 	} catch(err) {
-// 		console.log('err 222222', err)
-// 	}
+// 		console.log('err', err)
+// 		return false;
+// 	} 
 // }
 
 module.exports = {
-	auth, init, newAuth
+	auth, init, newAuth, insertDataUser, getDataUser, 
+	getData, insertData, updateData, deleteData
 }
--- controllers/services/userService.js
+++ controllers/services/userService.js
@@ -1,27 +1,32 @@
 var async = require('async');
-var config = require('../../config');
+// var config = require('../../config');
+var config = require('../../config2');
 var appService = require('./appService');
 var _ = require('lodash');
 
 let getUsers = async(req, res) => {
 	try {
-		let dbCallback = await config.db('User');
-		dbCallback.orderByValue().on('value', (snapshot) => {
-			var arr = [];
-			snapshot.forEach((data) => {
-				var newData = { 
-					date_created: data.val().date_created,
-					name: data.val().name,
-					password: data.val().password,
-					username: data.val().username,
-					uid: data.key,
-					last_logged_in :  data.val().last_logged_in == null ? null : data.val().last_logged_in
-				};
-				arr.push(newData);
-			});
+		let arr = await config.getData('User')
+		res.status(200).json(appService.response(true, 'success', arr.key))
 
-			res.status(200).json(appService.response(true, 'success', arr))
-		})
+		// REALTIME
+		// let dbCallback = await config.db('User');
+		// dbCallback.orderByValue().on('value', (snapshot) => {
+		// 	var arr = [];
+		// 	snapshot.forEach((data) => {
+		// 		var newData = { 
+		// 			date_created: data.val().date_created,
+		// 			name: data.val().name,
+		// 			password: data.val().password,
+		// 			username: data.val().username,
+		// 			uid: data.key,
+		// 			last_logged_in :  data.val().last_logged_in == null ? null : data.val().last_logged_in
+		// 		};
+		// 		arr.push(newData);
+		// 	});
+
+		// 	res.status(200).json(appService.response(true, 'success', arr))
+		// })
 	} catch (err) {
 		console.log('err', err)
 		res.status(500).json(appService.response(false, 'Internal server error', null))
@@ -31,11 +36,16 @@ let getUsers = async(req, res) => {
 let deleteUser = async(req, res) => { 
 	try { 
 		var body = req.body;
-		var ref = await config.dbChild('User')
-		var dbCall = ref.child(body.uid);
-		dbCall.remove().then((result) => {
-			res.status(200).json(appService.response(true, 'removed', null))
-		})
+		let del = await config.deleteData('User', body.uid);
+		res.status(200).json(appService.response(true, 'removed', null))
+
+		// let del = await config.deleteData('User', body.uid);
+		// res.status(200).json(appService.response(true, 'removed', null))
+		// var ref = await config.dbChild('User')
+		// var dbCall = ref.child(body.uid);
+		// dbCall.remove().then((result) => {
+		// 	res.status(200).json(appService.response(true, 'removed', null))
+		// })
 	} catch(err) {
 		console.log('err', err)
 		res.status(500).json(appService.response(false, 'Internal server error', null))
